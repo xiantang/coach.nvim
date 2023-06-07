2f942de7ca4093170e16f5124806ecc1bfb43d2f
xiantang <zhujingdi1998@gmail.com>
2023-06-07 22:23:13 +0800

    Update install.sh script to install keylogger.

--- install.sh
+++ install.sh
@@ -11,8 +11,7 @@ which keylogger >/dev/null 2>&1
 if [ $? -eq 0 ]; then
 	echo "keylogger already installed"
 	exit 0
-else
-	echo "keylogger not installed"
-	echo "Installing keylogger"
-	sudo apt-get install keylogger
 fi
+
+echo "keylogger not installed"
+echo "Installing keylogger"
