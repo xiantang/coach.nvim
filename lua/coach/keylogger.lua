c3e76c18318e198a4e08de188c0f6c15c5ea99aa
xiantang <zhujingdi1998@gmail.com>
2023-06-07 21:58:13 +0800

    Add keylogger module to Lua plugin and add run and stop functions. Remove unused code.

---
+++ lua/coach/keylogger.lua
@@ -0,0 +1,25 @@
+-- module represents a lua module for the plugin
+
+local keylogger
+local M = {}
+
+function M.run(logfile)
+  if keylogger then
+    vim.fn.jobstop(keylogger)
+  end
+  local pid = vim.fn.getpid()
+  local args = { "keylogger", logfile }
+  keylogger = vim.fn.jobstart(args)
+end
+
+function M.stop(opts)
+  if keylogger then
+    vim.fn.jobstop(keylogger)
+  end
+end
+
+M.my_first_function = function()
+  print("hello")
+end
+
+return M
