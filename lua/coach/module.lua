5d16ace4765dfaad3625e4f8ddea665c66326bff
xiantang <zhujingdi1998@gmail.com>
2023-06-07 21:52:35 +0800

    Refactor my_first_function to be a commented out stub.

--- lua/coach/module.lua
+++ lua/coach/module.lua
@@ -1,8 +1,8 @@
 -- module represents a lua module for the plugin
 local M = {}
 
-M.my_first_function = function()
-  print("hello")
-end
+-- M.my_first_function = function()
+--   print("hello")
+-- end
 
 return M
