71b1117ba0cc079597470347d0b3111b93c5e7b4
xiantang <zhujingdi1998@gmail.com>
2023-06-07 22:06:16 +0800

    Add keylogger and start/stop commands for logging in Coach module and plugin.

--- lua/coach.lua
+++ lua/coach.lua
@@ -1,10 +1,12 @@
 -- main module file
-local module = require("coach.module")
-
 local M = {}
+local logger = require("coach.keylogger")
+M.last = ""
+M.logfile = vim.fn.expand(string.format("~/logfile_%s.log", vim.fn.getpid()))
+
 M.config = {
   -- default config
-  opt = "Hello!",
+  -- opt = "Hello!",
 }
 
 -- setup is the public method to setup your plugin
@@ -12,11 +14,17 @@ M.setup = function(args)
   -- you can define your setup function here. Usually configurations can be merged, accepting outside params and
   -- you can also put some validation here for those.
   M.config = vim.tbl_deep_extend("force", M.config, args or {})
+  vim.api.nvim_create_user_command("UShouldUseStart", function()
+    logger.run(M.logfile)
+  end, {})
+  vim.api.nvim_create_user_command("UShouldUseStop", function()
+    logger.stop()
+  end, {})
 end
 
 -- "hello" is a public method for the plugin
-M.hello = function()
-  module.my_first_function()
-end
+-- M.hello = function()
+--   module.my_first_function()
+-- end
 
 return M
--- plugin/coach.lua
+++ plugin/coach.lua
@@ -1,1 +1,9 @@
-vim.api.nvim_create_user_command("MyFirstFunction", require("coach").hello, {})
+-- vim.api.nvim_create_user_command("MyFirstFunction", require("coach").hello, {})
+
+vim.api.nvim_create_user_command("UShouldUseStart", function()
+  require("coach").logger.run()
+  -- logger.run(M.logfile)
+end, {})
+vim.api.nvim_create_user_command("UShouldUseStop", function()
+  require("coach").logger.stop()
+end, {})
