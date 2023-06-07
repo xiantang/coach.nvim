02d720ae43ee1b321025fc82c3df5f66d8511456
xiantang <zhujingdi1998@gmail.com>
2023-06-07 22:21:48 +0800

    Add keylogger installation script with check for existing installation.

--- install.sh
+++ install.sh
@@ -1,1 +1,16 @@
 #!/bin/bash
+
+echo "Run keylogger Installer"
+
+# check if exist using `which`
+
+which keylogger >/dev/null 2>&1
+
+if [ $? -eq 0 ]; then
+	echo "keylogger already installed"
+	exit 0
+else
+	echo "keylogger not installed"
+	echo "Installing keylogger"
+	sudo apt-get install keylogger
+fi
