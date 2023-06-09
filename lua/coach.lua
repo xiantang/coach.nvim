a0923dd7577fa01b4a1a07294b6e903b8684ed97
xiantang <zhujingdi1998@gmail.com>
2023-06-09 11:50:36 +0800

    Removed TextYankPost autocmd in coach.lua.

--- lua/coach.lua
+++ lua/coach.lua
@@ -79,14 +79,6 @@ M.setup = function(args)
       end
     end,
   })
-  vim.api.nvim_create_autocmd({ "TextYankPost" }, {
-    pattern = { "*" },
-    callback = function()
-      vim.highlight.on_yank({
-        timeout = 300,
-      })
-    end,
-  })
 end
 
 return M
