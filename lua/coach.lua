5fed53b9baffd7468dc80542649057c83140885d
xiantang <zhujingdi1998@gmail.com>
2023-06-22 13:28:25 +0800

    Add timeout of 500 milliseconds in coach.lua.

--- lua/coach.lua
+++ lua/coach.lua
@@ -34,6 +34,7 @@ M.setup = function(args)
             notify(s, vim.log.levels.WARN, {
               title = "coach.nvim",
               render = "compact",
+              timeout = 500,
               on_open = function(win)
                 local buf = vim.api.nvim_win_get_buf(win)
                 vim.api.nvim_buf_set_option(buf, "filetype", "markdown")
